82035b0a351ad4cefc1ba6cf66883b46bb0de91b
Donatas Vidutis <donatasvidutis@gmail.com>
2022-12-07 15:36:31 +0300

    Project creation

---
+++ ImageViewer/ImageViewer/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  ImageViewer
+//
+//  Created by Donatas on 7.12.22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
